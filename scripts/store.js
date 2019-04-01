47591af7f51669a4a547fcea112d50f4ff8904da
Ben Tilghman <ben.tilghman@gmail.com>
2019-04-01 16:50:03 -0400

    done!

--- scripts/store.js
+++ scripts/store.js
@@ -9,17 +9,45 @@ const store = (function() {
         { id: cuid(), name: 'bread', checked: false }
     ];
 
-    const findByID = function(id) {
+    const findById = function(id) {
         return store.items.find(item => item['id'] === id);
     };
 
     const addItem = function(name) {
+        console.log('adding item');
         try {
             Item.validateName(name);
             this.items.push(Item.create(name));
         } catch(error) {
             console.log(`Cannot add item:${error.message}`);
         }
+    };
+
+    const findAndToggleChecked = function(id) {
+        const item = findById(id);
+        item.checked = !item.checked;       
+    }
+
+    const findAndUpdateName = function(id, newName) {
+        try {
+            Item.validateName(newName);
+            const item = findById(id);
+            item.name = newName;
+        } catch(error) {
+            console.log(`Cannot update name: ${error.message}`)
+        }
+    };
+
+    const findAndDeleteName = function(id) {
+        this.items = this.items.filter(item => item.id !== id);
+    };
+
+    const toggleCheckedFilter = function() {
+        this.hideCheckedItems = !this.hideCheckedItems;
+    };
+
+    const setSearchTerm = function(term) {
+        this.searchTerm = term;
     };
 
     const hideCheckedItems = false;
@@ -30,6 +58,13 @@ const store = (function() {
         items,
         hideCheckedItems,
         searchTerm,
-    }
+        findById,
+        addItem,
+        findAndUpdateName,
+        findAndDeleteName,
+        findAndToggleChecked,
+        toggleCheckedFilter,
+        setSearchTerm,
+    };
 
 } () );
